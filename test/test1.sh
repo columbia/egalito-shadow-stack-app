248a5a787a8543bdab381b030e0c87562801426a
Egalito User <ubuntu@localhost>
2021-10-24 05:24:37 +0000

    add test1, test2 scripts to make testing simpler

---
+++ test/test1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+echo
+echo ========= Invoking 1-1 Egalito transformation
+../egalito/app/etelf vuln vuln.m
+
+echo
+echo ========= Invoking shadow-stack app
+../app/etapp -q vuln vuln.ss
+
+echo
+echo ========= Original program
+perl vuln.pl ./vuln
+echo "^^^ should have been successfully exploited (status 1)"
+
+echo
+echo ========= Egalito-hardened program
+perl vuln.pl ./vuln.m
+echo "^^^ should have been successfully exploited (status 1)"
+
+echo
+echo ========= Shadow-stack-hardened program
+perl vuln.pl ./vuln.ss
+
+echo "^^^ should have died with status 4"
